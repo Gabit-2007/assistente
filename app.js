d9a76d672faaed08417c0d434c53d3e1572a9582
kkLari22194 <ycrokidopena@gmail.com>
2025-06-13 15:28:24 -0300

    a

--- app.js
+++ app.js
@@ -1,26 +1,45 @@
 import * as funcoes from './function.js';
 
 let escolha;
+let escolha2;
 let nome = funcoes.Nome();
 funcoes.Apresentacao();
 
 escolha = funcoes.PedirEscolha(nome);
 
-while( escolha =! 0){
+while( escolha != 0){
     if(escolha == 1){
         console.log('Certo, ' + nome + '! Você escolheu fazer um orçamento!')
         funcoes.Cardapio();
 
-        let escolha2 = funcoes.PedirEscolha2(nome);
+        escolha2 = funcoes.PedirEscolha2(nome);
         let qtd = funcoes.Perguntar(nome);
         let preco = [0, 8, 4, 6, 10, 6, 8, 6, 4, 2, 4];
 
-        total = funcoes.Soma(escolha2, qtd, preco);
+        let total = funcoes.Soma(escolha2, qtd, preco);
+
+        console.log('Seu pedido deu R$' + total);
+
     }
 
     else if(escolha == 2){
         console.log('Certo, ' + nome + '! Você escolheu saber mais sobre nossas sobremesas!');
         funcoes.Cardapio();
-        escolha = funcoes.PedirEscolha;
+        escolha2 = funcoes.PedirEscolha2(nome);
+        let resul= funcoes.Ingredientes(escolha2);
+
+        console.log(resul);
     }
+
+    else if(escolha == 3){
+        console.log('Certo, ' + nome + '! Você escolheu saber mais sobre os personagens!');
+        funcoes.Personagens();
+        escolha2 = funcoes.PedirEscolha2(nome);
+        let resul = funcoes.Curiosidades(escolha2);
+
+        console.log(resul);
+    }
+    
+    funcoes.Apresentacao();
+    escolha = funcoes.PedirEscolha(nome);
 }
--- function.js
+++ function.js
@@ -55,12 +55,71 @@ export function Soma(escolha2, qtd, preco){
 }
 
 export function Ingredientes(escolha2){
-    if(escolha == 1){
-        console.log('- Massa de baunilha');
-        console.log('- Corante rosa para dar a cor de bubblegum');
-        console.log('- Cobertura de chantilly');
-        console.log('- Confeitos coloridos');
-        console.log('- Chocolate meio amargo picado para o toque final');
+    if(escolha2 == 1){
+        return 'Massa de baunilha, Corante rosa para dar a cor de bubblegum, Cobertura de chantilly, Confeitos coloridos, Chocolate meio amargo picado para o toque final';
     }
-    else if(escolha == 2){}
+    else if(escolha2 == 2){
+        return 'Frutas vermelhas (morango e framboesa) para o rubi, Frutas azuis (mirtilo ou amoras) para a safira, Açúcar, Gelatina em pó';
+    }
+    else if(escolha2 == 3){
+        return 'Gelatina (de diversos sabores roxos, como uva e amora), Açúcar, Amido de milho, Corante roxo';
+    }
+    else if(escolha2 == 4){
+        return 'Biscoitos de graham ou bolo de bolacha para a base, Cream cheese, Creme de leite, Gelatina rosa (ou corante rosa), Frutas do bosque (como framboesa) para decoração';
+    }
+    else if(escolha2 == 5){
+        return 'Chocolate meio amargo, Manteiga, Ovos, Farinha, Nozes (opcional)';
+    }
+    else if(escolha2 == 6){
+        return 'Chocolate branco (para o coating), Pistaches triturados, Palitos de pirulito, Corante verde (opcional, para intensificar a cor)';
+    }
+    else if(escolha2 == 7){
+        return 'Bolo de baunilha, Cobertura de manteiga azul, Confeitos em formato de gota, Geléia de blueberry para um toque a mais';
+    }
+    else if(escolha2 == 8){
+        return 'Chocolate chip cookies, Fondant rosa (para criar a forma de espada), Confeitos prateados (para detalhes), Açúcar de confeiteiro (para polvilhar)';
+    }
+    else if(escolha2 == 9){
+        return 'Marshmallows, Corante rosa e preto, Açúcar de confeiteiro, Palitos de churrasco (para as "twists")';
+    }
+    else if(escolha2 == 10){
+        return 'Farinha de amêndoa, Açúcar de confeiteiro, Clara de ovo, Corante azul (para combinar com a Pérola), Recheio de creme de manteiga de baunilha ou ganache de chocolate branco';
+    }
+    else{
+        return 'escolha inválida';
+    }
+}
+
+export function Personagens(){
+    console.log('\n - Personagens:');
+    console.log('1 - Steven');
+    console.log('2 - Pérola');
+    console.log('3 - Garnet');
+    console.log('4 - Ametista');
+    console.log('5 - Rose');
+    console.log('6 - Greg');
+    console.log('7 - Peridot');
+    console.log('8 - Lapis');
+    console.log('9 - Connie');
+    console.log('10 - Espinela');
+}
+
+export function Curiosidades(escolha2){
+    if(escolha2 == 1){
+        return 'Você sabia que Steven é baseado em uma pessoa real: O personagem foi inspirado no irmão mais novo da criadora da série, Steven Sugar. Ele também trabalhou na produção da série como designer de cenários';
+ }
+    else if(escolha2 == 2){
+        return 'Apesar de ser uma Gem “de apoio”, Pérola demonstra grande conhecimento técnico, engenharia de naves e habilidades de pilotagem. Ela frequentemente conserta dispositivos, ativa portais e entende a tecnologia das Gems melhor que muitas guerreiras.';
+ }
+ else if(escolha2 == 3){
+     return 'Ela geralmente usa óculos futuristas, mas quando tira, revela três olhos — resultado da fusão de Ruby e Sapphire. Cada olho representa a perspectiva individual das duas Gemas, e o terceiro representa sua visão compartilhada como Garnet';
+}
+else if(escolha == 4){
+    return 'Ametista foi formada no planeta Terra, mas saiu menor e menos poderosa do que o esperado para uma Gema da sua espécie. Isso faz com que ela, às vezes, se sinta inferior às outras Crystal Gems, principalmente à Garnet e à Pérola';
+}
+else if (escolha2 == 5){
+    return 'Rose era fascinada pelos humanos e pelo livre-arbítrio, o que a fez se apaixonar pelo planeta Terra. Ela acreditava no valor da imperfeição, da mudança e da liberdade — o que motivou sua rebelião contra o sistema das Gemas.';
+}
+else if(escolha2 == 6){
+    return 
 }
